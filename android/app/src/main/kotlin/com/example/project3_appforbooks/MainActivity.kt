8db94cf85c146aeca62e28056c6811fdcc4dde23
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-12-07 12:11:51 -0600

    Project Created

---
+++ android/app/src/main/kotlin/com/example/project3_appforbooks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project3_appforbooks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/auth/screens/signup_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SingUpScreen extends StatefulWidget {
+  const SingUpScreen({super.key});
+
+  @override
+  State<SingUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SingUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Column(children: []),
+        ),
+      ),
+    );
+  }
+}
