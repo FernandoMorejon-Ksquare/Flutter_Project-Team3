1cb59dc07afc57d6a69e8d828774e35955207cd8
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-12-08 13:42:43 -0600

    0.1 testing1

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -11,9 +11,29 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("ALEXANDRIA"),
+        centerTitle: true,
+      ),
       body: SafeArea(
         child: Container(
-          child: Column(children: []),
+          child: Column(children: [
+            const Text("All Books and E-Books"),
+            Image.asset(
+              "Flutter_Project-Team3/lib/core/common/assets/Portrait.jpg",
+              height: 200,
+            ),
+            const Text("Email"),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const Text("Password"),
+            TextFormField(
+              obscureText: true,
+            ),
+            ElevatedButton(onPressed: () {}, child: const Text("Sign Up")),
+            ElevatedButton(onPressed: () {}, child: const Text("Log In"))
+          ]),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project3_appforbooks/features/auth/screens/login_screen.dart';
 import 'package:project3_appforbooks/home.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
