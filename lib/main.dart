857893377bfaf0f3cd50536c8ba756502661776c
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-12-15 22:43:13 -0600

    update5

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Alexandria Books',
       theme: CustomTheme().theme,
-      home: const ProfileScreen(),
+      home: const BookDetailsScreen(),
     );
   }
 }
