0db169d3da65654c567645631d1e30f25d7c59c7
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2022-12-30 11:19:55 -0600

    quickfix2

--- lib/features/user/profile_screen.dart
+++ lib/features/user/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:project3_appforbooks/features/books/screens/favorites_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -14,73 +13,74 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.arrow_back),
-        centerTitle: true,
-        title: const Text("Profile Screen"),
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            margin: const EdgeInsets.only(left: 24.0, right: 24.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const SizedBox(
-                height: 64.0,
-              ),
-              Image.asset("assets/Portrait.jpg", fit: BoxFit.fitWidth),
-              const SizedBox(
-                height: 64,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 32.0),
-                child: Text(
-                  "First Name",
-                  style: TextStyle(fontSize: 48.0),
-                ),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 32.0),
-                child: Text(
-                  "Last Name",
-                  style: TextStyle(fontSize: 48.0),
-                ),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 32.0),
-                child: Text(
-                  "Email",
-                  style: TextStyle(fontSize: 48.0),
-                ),
-              ),
-              const SizedBox(
-                height: 16.0,
-              ),
-              Container(
-                  height: 50,
-                  width: double.infinity,
-                  margin:
-                      const EdgeInsets.only(left: 32.0, right: 32.0, top: 16.0),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacementNamed(
-                            context, FavoriteScreen.routeName);
-                      },
-                      child: const Text(
-                        "Go to Favorites",
-                        style: TextStyle(
-                            fontSize: 24.0, fontWeight: FontWeight.bold),
-                      ))),
-            ]),
-          ),
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back),
+          centerTitle: true,
+          title: const Text("Profile Screen"),
         ),
-      ),
-    );
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.only(left: 24.0, right: 24.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 64.0,
+                    ),
+                    Image.asset("assets/Portrait.jpg", fit: BoxFit.fitWidth),
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 32.0),
+                      child: Text(
+                        "First Name",
+                        style: TextStyle(fontSize: 48.0),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 32.0),
+                      child: Text(
+                        "Last Name",
+                        style: TextStyle(fontSize: 48.0),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 32.0),
+                      child: Text(
+                        "Email",
+                        style: TextStyle(fontSize: 48.0),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16.0,
+                    ),
+                    //   Container(
+                    //       height: 50,
+                    //       width: double.infinity,
+                    //       margin:
+                    //           const EdgeInsets.only(left: 32.0, right: 32.0, top: 16.0),
+                    //       child: ElevatedButton(
+                    //           onPressed: () {
+                    //             Navigator.pushReplacementNamed(
+                    //                 context, FavoriteScreen.routeName);
+                    //           },
+                    //           child: const Text(
+                    //             "Go to Favorites",
+                    //             style: TextStyle(
+                    //                 fontSize: 24.0, fontWeight: FontWeight.bold),
+                    //           ))),
+                    // ]),
+                  ]),
+            ),
+          ),
+        ));
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:project3_appforbooks/features/books/screens/book_screen.dart';
-import 'package:project3_appforbooks/features/books/screens/favorites_screen.dart';
+
 import 'package:project3_appforbooks/features/main/screens/home_screen.dart';
 import 'package:project3_appforbooks/features/user/profile_screen.dart';
 
-import '../features/auth/screens/login_screen.dart';
-import '../features/auth/screens/register_screen.dart';
+import 'features/auth/screens/login_screen.dart';
+import 'features/auth/screens/register_screen.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   final name = settings.name;
