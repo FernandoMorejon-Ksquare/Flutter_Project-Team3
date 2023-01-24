7f36fa5dc662b79c97ef2b09b915fe840224f664
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2023-01-24 02:48:41 -0600

    major update part2

--- lib/features/auth/screens/register_screen.dart
+++ lib/features/auth/screens/register_screen.dart
@@ -217,7 +217,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   width: double.infinity,
                   margin: const EdgeInsets.only(left: 32, right: 32, top: 16),
                   child: ElevatedButton(
-                      style: ButtonStyle(),
+                      style: isEnabled
+                          ? ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                              Colors.grey,
+                            ))
+                          : null,
                       onPressed: isEnabled
                           ? () {
                               if (passMatcher == passMatcher2) {
