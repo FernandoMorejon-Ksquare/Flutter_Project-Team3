af085bd5d0ba7ee080d612f6e799154632055385
Fernando Morejon <Fernando.Morejon@itksquare.edu.mx>
2023-01-27 02:20:51 -0600

    fix1

--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:form_field_validator/form_field_validator.dart';
+
 import 'package:project3_appforbooks/features/auth/controller/logreg_provider.dart';
 import 'package:project3_appforbooks/features/auth/controller/validation.dart';
 import 'package:project3_appforbooks/features/auth/screens/register_screen.dart';
@@ -54,8 +54,6 @@ class LoginScreenState extends State<LoginScreen> {
                 Container(
                   margin: const EdgeInsets.only(right: 32, left: 32),
                   child: TextFormField(
-                    validator:
-                        EmailValidator(errorText: 'Enter a valid Email Adress'),
                     controller: _emailCtrl,
                     onChanged: (email) {
                       AuthServiceProvider().validateEmail(email);
@@ -95,7 +93,6 @@ class LoginScreenState extends State<LoginScreen> {
                 Container(
                   margin: const EdgeInsets.only(right: 32, left: 32),
                   child: TextFormField(
-                    validator: passwordValidator,
                     controller: _passwordCtrl,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
